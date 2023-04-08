7013553f406dd3968dff3c14531da3a1e99d839c
Nakuru Wubni <nwubni@gmail.com>
2023-04-08 16:49:20 +0100

    Adjusted the interface and added some JavaScript logic for theme switching

--- popup.js
+++ popup.js
@@ -1,33 +1,46 @@
-if (document.querySelector(".popup")) {
-    const button = document.querySelector(".button");
-    const circle = document.querySelector(".circle")
-    let buttonOn = false;
-    function invert() {
-        alert("hi")
+const themeSwitch = document.getElementById("theme-switch");
+const themeIcon = document.getElementById("theme-icon");
+const themeMessage = document.getElementById("theme-message");
+const contributeLink = document.getElementById("contribute-link");
+let darkModeOn = false;
+
+// Inverts the popup color
+function invert(darkModeOn) {
+    // If dark mode is applied
+    if (!darkModeOn) {
         document.body.style.filter = "invert(1) hue-rotate(180deg)";
         let media = document.querySelectorAll("img, picture, video");
         media.forEach((mediaItem) => {
             mediaItem.style.filter = "invert(1) hue-rotate(180deg)"
-        })
+        });
     }
-    button.addEventListener("click", () => {
-        if (!buttonOn) {
-            buttonOn = true;
-
-            button.style.animation = "transformToBlue 1s ease-in-out 0s forwards"
-            circle.style.animation = "moveCircleRight 1s ease-in-out 0s forwards"
-            chrome.tabs.executeScript({
-                file: 'appOn.js'
-            })
-        }
-        else {
-            buttonOn = false;
-            button.style.animation = "transformToYellow 1s ease-in-out 0s forwards"
-            circle.style.animation = "moveCircleLeft 1s ease-in-out 0s forwards"
-            chrome.tabs.executeScript({
-                file: 'appOff.js'
-            })
-        }
-    })
-
+    else {
+        document.body.style.filter = "invert(0)";
+        let media = document.querySelectorAll("img, picture, video");
+        media.forEach((mediaItem) => {
+            mediaItem.style.filter = "invert(0)"
+        });
+    }
 }
+
+themeSwitch.addEventListener("change", () => {
+    // If dark mode is applied
+    if (!darkModeOn) {
+        themeIcon.classList.remove("fa-sun");
+        themeIcon.classList.add("fa-moon");
+        themeMessage.textContent = "Switch Off Dark Mode";
+        contributeLink.classList.remove("contribute-link-light-mode");
+        contributeLink.classList.add("contribute-link-dark-mode");
+    }
+    else {
+        themeIcon.classList.remove("fa-moon");
+        themeIcon.classList.add("fa-sun");
+        themeMessage.textContent = "Switch On Dark Mode";
+        contributeLink.classList.remove("contribute-link-dark-mode");
+        contributeLink.classList.add("contribute-link-light-mode");
+    }
+
+    invert(darkModeOn);
+
+    darkModeOn = !darkModeOn;
+});
